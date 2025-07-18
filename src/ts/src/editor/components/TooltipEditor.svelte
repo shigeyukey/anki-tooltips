25e3741e585f7730a5e929f948c58a8e9fc7564d
shigeyukey <shigge.3.21@gmail.com>
2025-07-18 15:14:57 +0900

    update License info for ts files

--- src/ts/src/editor/components/TooltipEditor.svelte
+++ src/ts/src/editor/components/TooltipEditor.svelte
@@ -1,7 +1,20 @@
 <!--
 Anki Tooltips
-Copyright (C) 2023 Matthias Metelka
-License: GNU AGPL, version 3 or later; http://www.gnu.org/licenses/agpl.html
+Copyright (C) Matthias Metelka (kleinerpirat) 2023 <https://github.com/kleinerpirat>
+Copyright (C) Shigeyuki 2025 <http://patreon.com/Shigeyuki>
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU Affero General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Affero General Public License for more details.
+
+You should have received a copy of the GNU Affero General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 @component
 A simple `<div contenteditable>` to edit the data-tippy-content attribute of TooltipAnchor.
