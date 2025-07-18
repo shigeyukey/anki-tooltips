2e6b9f85b9a301db4ced6f04966525b70ee02400
shigeyukey <shigge.3.21@gmail.com>
2025-07-18 14:00:55 +0900

    update License info

--- src/python/config.py
+++ src/python/config.py
@@ -1,10 +1,23 @@
 # Anki Tooltips
-# Copyright (C) 2023 Matthias Metelka
-# License: GNU AGPL, version 3 or later; http://www.gnu.org/licenses/agpl.html
+# Copyright (C) Matthias Metelka (kleinerpirat) 2023 <https://github.com/kleinerpirat>
+# Copyright (C) Shigeyuki 2025 <http://patreon.com/Shigeyuki>
+#
+#ProfileConfig and ModelConfig from Closet add-on for Anki
+# Copyright (C) 2021 Henrik Giesel <https://github.com/hgiesel>
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-# ProfileConfig and ModelConfig from Closet add-on for Anki
-# Copyright (C) 2021 Henrik Giesel
-# License: GNU General Public License v3.0; https://www.gnu.org/licenses/gpl-3.0-standalone.html
 
 from aqt import mw
 from typing import Any, Optional, Literal
